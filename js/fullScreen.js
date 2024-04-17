54e7adde6b3d37a2697b7fede736bdaa9f56ca72
Andrei090 <andrew250903@mail.ru>
2024-04-18 02:28:19 +0300

    Opens and closes part2 task

--- js/fullScreen.js
+++ js/fullScreen.js
@@ -6,15 +6,15 @@ var commentsCount = bigPicture.querySelector('.comments-count');
 var socialComments = bigPicture.querySelector('.social__comments');
 var socialCaption = bigPicture.querySelector('.social__caption');
 var closeButton = bigPicture.querySelector('.big-picture__cancel');
+var commentCountBlock = bigPicture.querySelector('.social__comment-count');
+var commentsLoader = bigPicture.querySelector('.comments-loader');
 
-var renderBigPicture = function(photo) {
-    bigImg.src = photo.url;
-    likesCount.textContent = `Нравится: ${photo.likes}`;
-    commentsCount.textContent = `Комментарии: ${photo.comments.length}`;
-    socialCaption.textContent = photo.description;
+var displayedComments = 5; // Количество показываемых комментариев изначально
 
-    socialComments.innerHTML = '';
-    photo.comments.forEach(function(comment) {
+var renderComments = function(photo) {
+    var commentsToShow = photo.comments.slice(0, displayedComments); // Получаем первые 5 комментариев или меньше, если их меньше
+    socialComments.innerHTML = ''; // Очищаем контейнер для комментариев
+    commentsToShow.forEach(function(comment) {
         var commentElement = document.createElement('li');
         commentElement.className = 'social__comment';
         commentElement.innerHTML = `
@@ -23,6 +23,30 @@ var renderBigPicture = function(photo) {
         `;
         socialComments.appendChild(commentElement);
     });
+
+    // Обновляем счетчик показанных комментариев
+    commentCountBlock.textContent = `${displayedComments} из ${photo.comments.length}`;
+
+    // Если количество показанных комментариев меньше общего числа комментариев, показываем кнопку "Загрузить ещё"
+    if (displayedComments < photo.comments.length) {
+        commentsLoader.classList.remove('hidden');
+    } else {
+        commentsLoader.classList.add('hidden');
+    }
+};
+
+var loadMoreComments = function(photo) {
+    displayedComments += 5; // Увеличиваем количество показываемых комментариев на 5
+    renderComments(photo); // Перерисовываем комментарии
+};
+
+var renderBigPicture = function(photo) {
+    bigImg.src = photo.url;
+    likesCount.textContent = `Нравится: ${photo.likes}`;
+    commentsCount.textContent = `Комментарии: ${photo.comments.length}`;
+    socialCaption.textContent = photo.description;
+
+    renderComments(photo); // Вызываем функцию для отображения комментариев
 
     document.body.classList.add('modal-open');
     bigPicture.classList.remove('hidden');
@@ -47,3 +71,5 @@ document.addEventListener('keydown', function(e) {
         closeBigPicture();
     }
 });
+
+commentsLoader.addEventListener('click', function() { loadMoreComments(currentPhotoData); });
